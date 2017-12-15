dc2c28609ed91c6548b236050ea41612110470c9
Claudiu Saiz <claudiu.saiz@avira.com>
2017-12-15 16:17:37 +0200

    For testing clang-format

---
+++ clang-format/test.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+try
+{
+  ;
+}
+catch ()
+{
+}
+enum
+{
+  ENTRY_NEW,
+  ENTRY_EXISTS_SAME_SIZE,
+  ENTRY_EXISTS_DIFF_SIZE
+} entry_type = ENTRY_NEW;
+
+class A
+{
+};
+
+static void some_func()
+{
+  if (1 == 1)
+  {
+    for (;;)
+    {
+      switch ()
+      {
+      case 1:
+      {
+      }
+      }
+    }
+  }
+}
+
+int main()
+{
+  int aaa = 111111 + 22222 + 1 + 2 + 3 + 1 + 2 + 3 + 1 + 2 + 3 + 1 + 2 + 3 + 1 +
+            2 + 3 + 1 + 2 + 3 + 1 + 2 + 3;
+  return 0;
+}
